91f5ebc4a538a898f39a479fa64c6bd65076d8c7
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-12-06 22:15:23 -0800

    initial commit

---
+++ Youtube/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  Youtube
+//
+//  Created by Mayank Gandhi on 12/6/20.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ Youtube/Views/HomeFeed/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  Youtube
+//
+//  Created by Mayank Gandhi on 12/6/20.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
